3e6b8e9265e7ff8601085cbed821375d1f955342
Madeha Shahid <madehashahid@hotmail.com>
2018-10-17 14:41:56 -0400

    user data

---
+++ userData.js
@@ -0,0 +1,16 @@
+module.exports = {
+    users: [
+        {
+            username: "madheha",
+            password: "coolCat"
+        },
+        {
+            username: "madlol",
+            password: "catsRcool"
+        },
+        {
+            username: "angryLaugh",
+            password: "coldKitty"
+        }
+    ]
+}
